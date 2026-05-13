628f60e88b5785ac8499173b8dc281adb8359963
Amaryllis-MAC <16129207+Amaryllis-YJY@users.noreply.github.com>
2026-05-13 23:40:24 +0900

    HW4

--- 06_cuda/13_bucket_sort.cpp
+++ 06_cuda/13_bucket_sort.cpp
@@ -12,21 +12,32 @@ int main() {
   }
   printf("\n");
 
-  std::vector<int> bucket(range); 
-  for (int i=0; i<range; i++) {
-    bucket[i] = 0;
-  }
-  for (int i=0; i<n; i++) {
-    bucket[key[i]]++;
-  }
-  for (int i=0, j=0; i<range; i++) {
-    for (; bucket[i]>0; bucket[i]--) {
-      key[j++] = i;
-    }
-  }
+  int *d_key, *d_bucket, *d_prefix;
+  cudaMalloc(&d_key, n * sizeof(int));
+  cudaMalloc(&d_bucket, range * sizeof(int));
+  cudaMalloc(&d_prefix, range * sizeof(int));
+  cudaMemcpy(d_key, key.data(), n * sizeof(int), cudaMemcpyHostToDevice);
+
+  int blockSize = 256;
+
+  initBucket<<<(range + blockSize - 1) / blockSize, blockSize>>>(d_bucket, range);
+  countBuckets<<<(n + blockSize - 1) / blockSize, blockSize>>>(d_key, d_bucket, n);
+
+  thrust::device_ptr<int> dp_bucket(d_bucket);
+  thrust::device_ptr<int> dp_prefix(d_prefix);
+  thrust::exclusive_scan(dp_bucket, dp_bucket + range, dp_prefix);
+
+  writeback<<<(n + blockSize - 1) / blockSize, blockSize>>>(d_key, d_prefix, n, range);
+
+  cudaMemcpy(key.data(), d_key, n * sizeof(int), cudaMemcpyDeviceToHost);
 
   for (int i=0; i<n; i++) {
     printf("%d ",key[i]);
   }
   printf("\n");
+
+  cudaFree(d_key);
+  cudaFree(d_bucket);
+  cudaFree(d_prefix);
+  return 0;
 }
